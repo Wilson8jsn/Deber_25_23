d28d4980e64091dca287f945034122953e8aca9d
Wilson Castillo <dc105677543@gmail.com>
2023-01-27 09:25:27 -0500

    FIRST CMMIT

--- ejercicio12.js
+++ ejercicio12.js
@@ -1,4 +1,4 @@
-let nums = [2,3,5,7,9]
+let nums = [2,3,5,7,9,10]
 let M = 0;
 for (let i = 0;i < nums.length;i++ ) {
     if (M < nums [i]){
--- ejercicio6.js
+++ ejercicio6.js
@@ -1,17 +1,4 @@
 let world = ['a', 'o','l', 'h'];
 let aux = 0;
-for (let i = 0 ; i < world.length; i++){
-    if(world[0]=world[3]){
-        world[3]=aux
-    }
-    if(world[1]=world[1]){
-        world[1]=aux
-    } 
-    if(world[2]=world[2]){
-        world[2]=aux
-    }
-    if(world[3]=world[0]){
-        world[0]=aux
-    }
-}
+
     console.log("ordenada :")
